4ce1435fd2cc824a40b2677ea73c17c13b3a4351
DavidCerv <davcertor@gmail.com>
2026-02-04 12:53:33 -0600

    Fix: configurar basename en el Router para GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouterbasename="/cervantesd.github.io">
             <Routes>
               <Route path="/" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
