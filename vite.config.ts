34772015f1959589a0e046bbb2871c0a75e835bd
DavidCerv <davcertor@gmail.com>
2026-02-05 21:52:51 -0600

    Fix: base path para despliegue en Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/cervantesd.github.io/',
+  base: '/',
   plugins: [
     react(),
     mode === 'development' &&
