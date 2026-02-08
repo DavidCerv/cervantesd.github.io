e49e620f1729eb0c37725287ccb094c57fd07012
DavidCerv <davcertor@gmail.com>
2026-02-07 20:30:16 -0600

    Fix: remover basename para despliegue final en Netlify

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter 
-basename="/cervantesd.github.io">
+          <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
